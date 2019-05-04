7d20914b8213ae2fa90b4758ba299a9c5456369c
Maggie Fang <menogenfong@gmail.com>
2019-05-03 21:24:42 -0400

    comit

--- query.py
+++ query.py
@@ -2,7 +2,8 @@ from owner_elasticsearch import *
 from countminsketch import *
 import re
 from collections import Counter
-
+import sys
+import time
 
 def search(es_object, index_name, search):
     res = es_object.search(index=index_name, body=search)
@@ -23,19 +24,7 @@ def term_query(user):
 
 
 def bool_query(user, str):
-    return {'query': {'bool': {
-        'must': {
-            "term": {
-                "user": user
-            }
-        },
-        "must_not": {
-            'match': {'text': str}
-        },
-    }
-    }
-    }
-
+   return {"query": {"bool": {"must": { "term": { "user": user }}, "must_not": { "match": {"text":text}}}}}
 
 def tokenize(string):
     words = re.compile('(\w+)')
@@ -56,14 +45,35 @@ def conduct_countMinTopK(data, k):
 es = connect_elasticsearch()
 cm_sketch = CountMinSketch(1024, 3)
 if es is not None:
-    term_search = term_query("Trump")
-    bool_search = bool_query("SalGood69", "happy")
-    match_search = match_query("Trump")
-    print("\n*****result of term query********\n")
-    result1 = search(es, twt_index, json.dumps(term_search))
-    print("\n*****result of bool query********\n")
-    result2 = search(es, twt_index, json.dumps(bool_search))
-    print("\n*****result of match query*******\n")
-    result3 = search(es, twt_index, json.dumps(match_search))
-    print("\n*******top 5 using countmin algorithm\n")
-    conduct_countMinTopK(result3['hits']['hits'], 5)
+    print("1. do full text match query.\n 2. do term query.\n 3 do bool query.\n 4 conduct count min alogrithm find top 5.\n 0 exit\n")
+    while True:
+        cmd = input("enter choice:")
+        if (cmd == '1'):
+            text = input("enter the text you want to match:")
+            match_search = match_query(text)
+            print("\n*****result of match query*******\n")
+            search(es, twt_index, json.dumps(match_search))
+        elif(cmd == '2'):
+            text = input("enter username:")
+            term_search = term_query(text)
+            print("\n*****result of term query********\n")
+            search(es, twt_index, json.dumps(term_search))
+        elif(cmd == '3'):
+            user = input("must have user:")
+            text = input("must not have text:")
+            print("\n*****result of bool query ********\n")
+            bool_search = bool_query("charles_tindol", "Thank")
+            search(es, twt_index, json.dumps(bool_query))
+        elif(cmd == '4'):
+            cont = 0
+            while True:
+                if(cont> 100):
+                    break
+                match_search = match_query("Trump")
+                result = search(es, twt_index, json.dumps(match_search))
+                print("\n*******top 5 using countmin algorithm\n")
+                conduct_countMinTopK(result['hits']['hits'], 5)
+                cont=cont+1
+                time.sleep(2) # Delay 
+        else:
+            break
